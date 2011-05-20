7abf53d2576cc2ff10b455306b0c291cf7ec409b
Carlos Reyes <carlosreyes83@gmail.com>
2011-05-20 17:38:53 -0300

    elimine el archivo public/index.html Chapter 5 -  Routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Return a title on a per-page basis. Devuelve el titulo de cada pagina
+    def title               #definicion de metodo
+      base_title = "Ruby on Rails Tutorial Sample App"      
+      if @title.nil?    #si no tiene nada asignado
+        base_title      #entonces devuelve el titulo pelado
+      else
+        "#{base_title} | #{@title}" #sino concatena las strings
+      end
+    end
 end
