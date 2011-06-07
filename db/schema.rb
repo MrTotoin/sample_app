c2d4e0fc9d12fce6f52c1f644b9dc0b6a6a8a8ad
Carlos Reyes <carlosreyes83@gmail.com>
2011-06-07 20:21:17 -0300

    modifique algunos archivos para seguir al pie de la letra al tutorial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110607214421) do
+ActiveRecord::Schema.define(:version => 20110607231652) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
