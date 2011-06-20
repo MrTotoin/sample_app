e2b8054cb9e30db995c9fb8f146e33b4a7bb914c
Carlos Reyes <carlosreyes83@gmail.com>
2011-06-20 19:45:35 -0300

    termine el capitulo 11

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,6 +41,11 @@ class User < ActiveRecord::Base
      return user if user.has_password?(submitted_password)
    end
    
+   def feed
+       # This is preliminary. See Chapter 12 for the full implementation.
+       Micropost.where("user_id = ?", id)
+   end
+   
    private
 
      def encrypt_password
